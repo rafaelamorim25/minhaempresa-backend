d874f458e0cbe1d4cb61a23e1c93f5a91786af3f
rafaelamorim25 <rafaelamorimclash@gmail.com>
2020-10-07 15:03:19 -0300

    use case: Manter empresa - testando o REST

---
+++ src/main/java/br/com/minhaempresa/resources/ManterEmpresaResource.java
@@ -0,0 +1,16 @@
+package br.com.minhaempresa.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/minhaempresa")
+public class ManterEmpresaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String teste() {
+		return "O REST está funcionando";
+	}
+
+}
