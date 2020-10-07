3229b8beed2dafd2dde480b8fb58f0ce26807098
rafaelamorim25 <rafaelamorimclash@gmail.com>
2020-10-07 01:10:17 -0300

    Setup: Criação do projeto

---
+++ src/main/java/br/com/minhaempresa/MinhaempresaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.minhaempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaempresaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaempresaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/minhaempresa/MinhaempresaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.minhaempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaempresaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
